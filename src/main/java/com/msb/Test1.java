74740f7775af02c14c5600040fe3acec4b61dc2f
yuanwt <18317509809@163.com>
2020-08-16 13:34:31 +0800

    测试test war项目上传

---
+++ src/main/java/com/msb/Test1.java
@@ -0,0 +1,15 @@
+package com.msb;
+
+
+/**
+ * @Auther:yuanwt
+ * @Date:2020/8/16 - 08 - 16 - 13:33
+ * @Description: com.msb
+ * @Version: 1.0
+ */
+public class Test1 {
+    public static void main(String[] args) {
+        System.out.println("hello");
+
+    }
+}
